7708c15bf71eb6cd47abf658be505933621563e4
Petr Homola <homola.p@gmail.com>
2023-03-27 15:40:11 +0100

    improved reduce function type

--- mapreduce/mapreduce.go
+++ mapreduce/mapreduce.go
@@ -3,7 +3,7 @@ package mapreduce
 // Engine is a wrapper for map-reduce algorithms.
 type Engine[T, V1, V2 any, K1, K2 comparable] struct {
 	f1 func(T, func(K1, V1))
-	f2 func(K1, []V1) (K2, V2)
+	f2 func(K1, []V1, func(K2, V2))
 }
 
 // Run runs the algorithm.
@@ -17,16 +17,29 @@ func (e *Engine[T, V1, V2, K1, K2]) Run(input []T) map[K2]V2 {
 	}
 	r := make(map[K2]V2)
 	for k, l := range pairs {
-		k, v := e.f2(k, l)
-		r[k] = v
+		e.f2(k, l, func(k K2, v V2) {
+			r[k] = v
+		})
 	}
 	return r
 }
 
 // New creates a new map-reduce engine.
-func New[T, V1, V2 any, K1, K2 comparable](f1 func(T, func(K1, V1)), f2 func(K1, []V1) (K2, V2)) *Engine[T, V1, V2, K1, K2] {
+func New[T, V1, V2 any, K1, K2 comparable](f1 func(T, func(K1, V1)), f2 func(K1, []V1, func(K2, V2))) *Engine[T, V1, V2, K1, K2] {
 	return &Engine[T, V1, V2, K1, K2]{
 		f1: f1,
 		f2: f2,
 	}
 }
+
+// type pair[T, U any] struct {
+// 	fst T
+// 	snd U
+// }
+
+// func newPair[T, U any](fst T, snd U) pair[T, U] {
+// 	return pair[T, U]{
+// 		fst: fst,
+// 		snd: snd,
+// 	}
+// }
