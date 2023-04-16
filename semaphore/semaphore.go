68608f3a122e2055ac071ccd07f7a497035a0c6a
Petr Homola <phomola@me.com>
2023-04-16 15:30:57 +0100

    Semaphores (#13)
    * Create semaphore.go
    
    * Update semaphore.go

---
+++ semaphore/semaphore.go
@@ -0,0 +1,24 @@
+package semaphore
+
+// Semaphore is a semaphore for synchronisation.
+type Semaphore struct {
+	ch chan struct{}
+}
+
+// New creates a new semaphore with an initial value.
+func New(val int) *Semaphore {
+	return &Semaphore{
+		ch: make(chan struct{}, val),
+	}
+}
+
+// Wait decreases the semaphore value, waiting if it gets beneath zero.
+func (s *Semaphore) Wait() {
+	s.ch <- struct{}{}
+}
+
+// Signal increases the semaphore value.
+func (s *Semaphore) Signal() {
+	<-s.ch
+}
+
