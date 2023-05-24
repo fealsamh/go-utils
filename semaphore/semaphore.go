a201066db28bdbe208262ed08037cde544020da7
Petr Homola <homola.p@gmail.com>
2023-05-24 13:00:08 +0100

    added semaphore WaitCtx

--- semaphore/semaphore.go
+++ semaphore/semaphore.go
@@ -1,5 +1,7 @@
 package semaphore
 
+import "context"
+
 // Semaphore is a semaphore for synchronisation.
 type Semaphore struct {
 	ch chan struct{}
@@ -17,8 +19,18 @@ func (s *Semaphore) Wait() {
 	s.ch <- struct{}{}
 }
 
+// WaitCtx decreases the semaphore value, waiting if it gets beneath zero.
+// The provided context can be used to stop waiting.
+func (s *Semaphore) WaitCtx(ctx context.Context) error {
+	select {
+	case s.ch <- struct{}{}:
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
+
 // Signal increases the semaphore value.
 func (s *Semaphore) Signal() {
 	<-s.ch
 }
-
