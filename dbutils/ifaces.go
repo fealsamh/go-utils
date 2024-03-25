937162b4db7ee405319c5ce10256ba0e9903b072
Petr Homola <homola.p@gmail.com>
2024-03-25 09:19:06 +0000

    DB interfaces

---
+++ dbutils/ifaces.go
@@ -0,0 +1,27 @@
+package dbutils
+
+import (
+	"context"
+	"database/sql"
+)
+
+// Querier is an interface for database queries.
+type Querier interface {
+	QueryContext(context.Context, string, ...interface{}) (*sql.Rows, error)
+}
+
+// Execer is an interface for database statements.
+type Execer interface {
+	ExecContext(context.Context, string, ...interface{}) (sql.Result, error)
+}
+
+// Txer is an interface for database transaction.
+type Txer interface {
+	BeginTx(context.Context, *sql.TxOptions) (*sql.Tx, error)
+}
+
+var (
+	_ Querier = (*sql.DB)(nil)
+	_ Execer  = (*sql.DB)(nil)
+	_ Txer    = (*sql.DB)(nil)
+)
