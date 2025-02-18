f7ed42b87cc15c5e52e15b6ab9e59ea4b8e036b0
Petr Homola <homola.p@gmail.com>
2025-02-18 11:16:01 +0000

    unwrapping errors

--- errors/errors.go
+++ errors/errors.go
@@ -34,17 +34,51 @@ func (e *Error) GRPCError() error {
 
 // ConvertToGRPC converts the error into a gRPC error.
 func ConvertToGRPC(err error) error {
-	if err, ok := err.(*Error); ok {
-		return err.GRPCError()
+	if err := convertToGRPC(err); err != nil {
+		return err
 	}
 	return status.Error(codes.Internal, err.Error())
 }
 
+func convertToGRPC(err error) error {
+	if err, ok := err.(*Error); ok {
+		return err.GRPCError()
+	}
+	if err, ok := err.(wrappedError); ok {
+		return convertToGRPC(err.Unwrap())
+	}
+	if err, ok := err.(wrappedErrors); ok {
+		for _, err := range err.Unwrap() {
+			if err := convertToGRPC(err); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
 // WriteHTTPHeader writes the HTTP header.
 func WriteHTTPHeader(err error, w http.ResponseWriter) {
-	if err, ok := err.(*Error); ok {
-		err.WriteHTTPHeader(w)
+	if s, ok := httpStatus(err); ok {
+		w.WriteHeader(s)
 	} else {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
+}
+
+func httpStatus(err error) (int, bool) {
+	if err, ok := err.(*Error); ok {
+		return err.code.HTTPStatus(), true
+	}
+	if err, ok := err.(wrappedError); ok {
+		return httpStatus(err.Unwrap())
+	}
+	if err, ok := err.(wrappedErrors); ok {
+		for _, err := range err.Unwrap() {
+			if s, ok := httpStatus(err); ok {
+				return s, true
+			}
+		}
+	}
+	return 0, false
 }
