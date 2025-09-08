0fd43ae89f7f3d1421393b82ccce8b3f121445c0
Petr Homola <homola.p@gmail.com>
2025-09-08 08:21:14 +0100

    +mobile, read resource as string

--- mobile/file.go
+++ mobile/file.go
@@ -3,10 +3,11 @@ package mobile
 import (
 	"io"
 
+	"github.com/fealsamh/go-utils/nocopy"
 	"golang.org/x/mobile/asset"
 )
 
-// GetFileData returns the contents of a resource file.
+// GetFileData returns the contents of a resource file as a slice of bytes.
 func GetFileData(filename string) ([]byte, error) {
 	f, err := asset.Open(filename)
 	if err != nil {
@@ -14,4 +15,13 @@ func GetFileData(filename string) ([]byte, error) {
 	}
 	defer f.Close()
 	return io.ReadAll(f)
+}
+
+// GetFileString returns the contents of a resource file as a string.
+func GetFileString(filename string) (string, error) {
+	b, err := GetFileData(filename)
+	if err != nil {
+		return "", err
+	}
+	return nocopy.String(b), nil
 }
