5ecbbaf4e65ff348bd96914f74ed6097f7fcaa52
Petr Homola <homola.p@gmail.com>
2026-07-30 01:03:35 +0100

    continuations

--- continuation/continuation_test.go
+++ continuation/continuation_test.go
@@ -1,7 +1,7 @@
 package continuation
 
 import (
-	"errors"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,10 +10,32 @@ import (
 func TestContinuation(t *testing.T) {
 	req := require.New(t)
 
-	res, err := Go(func(cont *Continuation[string]) {
-		cont.Resume("result", errors.New("error"))
+	// unit
+	c := Unit[int](1234)
+	var s string
+	r := c(func(x int) int {
+		s = strconv.Itoa(x)
+		return x + 1
 	})
+	req.Equal(1235, r)
+	req.Equal("1234", s)
 
-	req.Equal("result", res)
-	req.Equal("error", err.Error())
+	// bind
+	c2 := c.Bind(func(x int) Continuation[float64, int] { return Unit[int](float64(x) + 1) })
+	var y float64
+	r = c2(func(x float64) int {
+		y = x
+		return int(x + 1)
+	})
+	req.Equal(1236, r)
+	req.Equal(1235.0, y)
+
+	// fmap
+	c3 := c.Fmap(func(x int) float64 { return float64(x) + 2 })
+	r = c3(func(x float64) int {
+		y = x
+		return int(x + 1)
+	})
+	req.Equal(1237, r)
+	req.Equal(1236.0, y)
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/fealsamh/go-utils
 
-go 1.26.5
+go 1.27
 
 require (
 	github.com/google/jsonschema-go v0.4.3
