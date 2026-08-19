1c81c148f574bc3740c6fe4a0fbb5cdd3f91820d
Petr Homola <homola.p@gmail.com>
2026-08-19 21:00:51 +0100

    Go 1.27 update (#117)
    * JSONv2 & UUID (#103)
    
    * jsnv2
    
    * Update test.yaml
    
    * deps
    
    * tidied
    
    * tests-update
    
    * Update go.mod
    
    * Update test.yaml
    
    * stdlib uuid
    
    * go fixed
    
    * uuid error message
    
    * jsonv2 syntactic error
    
    * Update test.yaml
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * Continuation — generic methods (#116)
    
    * deps
    
    * deps
    
    * deps
    
    * update test go versions
    
    * deps
    
    * deps
    
    * deps
    
    * +mobile, read resource as string
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps'
    
    * deps
    
    * deps
    
    * godoc
    
    * deps
    
    * deps
    
    * sums
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * Update test.yaml
    
    * deps
    
    * deps
    
    * deps
    
    * tidied
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * go version update
    
    * deps
    
    * deps
    
    * deps
    
    * updated go version
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * continuations
    
    * Update test.yaml
    
    * Update test.yaml
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * Update test.yaml
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * deps
    
    * Update test.yaml

--- continuation/continuation.go
+++ continuation/continuation.go
@@ -1,29 +1,34 @@
 package continuation
 
-import "sync"
+import "github.com/fealsamh/go-utils/function"
 
 // Continuation is a continuation.
-type Continuation[T any] struct {
-	wg     *sync.WaitGroup
-	result T
-	err    error
-}
+type Continuation[T, R any] func(func(T) R) R
 
-// Resume resumes the continuation.
-func (c *Continuation[T]) Resume(result T, err error) {
-	c.result = result
-	c.err = err
-	c.wg.Done()
-}
-
-// Go creates a continuation.
-func Go[T any](f func(*Continuation[T])) (T, error) {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	cont := &Continuation[T]{
-		wg: &wg,
+// Unit ...
+func Unit[R, T any](x T) Continuation[T, R] {
+	return func(f func(T) R) R {
+		return f(x)
 	}
-	go f(cont)
-	wg.Wait()
-	return cont.result, cont.err
+}
+
+// Bind ...
+func (c Continuation[T, R]) Bind[U any](f func(T) Continuation[U, R]) Continuation[U, R] {
+	return func(g func(U) R) R {
+		return c(func(x T) R {
+			return f(x)(g)
+		})
+	}
+}
+
+// Fmap ...
+func (c Continuation[T, R]) Fmap[U any](f func(T) U) Continuation[U, R] {
+	return c.Bind(func(x T) Continuation[U, R] {
+		return Unit[R](f(x))
+	})
+}
+
+// Join ...
+func Join[T, R any](c Continuation[Continuation[T, R], R]) Continuation[T, R] {
+	return c.Bind(function.Identity)
 }
